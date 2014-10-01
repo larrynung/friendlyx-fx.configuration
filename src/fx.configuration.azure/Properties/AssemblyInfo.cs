c77b9661705d169c162280eaa10f0863fedda6d8
dmitrygaraev <dmitry.garaev@live.com>
2014-10-01 00:54:38 -0700

    Redefining solution folder structure and projects to support different net framewrok versions

---
+++ src/fx.configuration.azure/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("FX.Configuration.Azure")]
+[assembly: AssemblyDescription("FX Configuration Azure")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FriendlyX")]
+[assembly: AssemblyProduct("FX.Configuration.Azure")]
+[assembly: AssemblyCopyright("FriendlyX Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.10.0")]
+[assembly: AssemblyFileVersion("0.1.10.0")]
--- src/fx.configuration.tests/Properties/AssemblyInfo.cs
+++ src/fx.configuration.tests/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1.6.0")]
-[assembly: AssemblyFileVersion("0.1.6.0")]
+[assembly: AssemblyVersion("0.1.10.0")]
+[assembly: AssemblyFileVersion("0.1.10.0")]
